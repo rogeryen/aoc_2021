f2a5b0d46ebaffcb4dad03ad5d07c3e408cfc5e0
Roger yen <rogeryen@gmail.com>
2021-12-02 21:33:33 -0500

    day 1b

--- src/01/solution.test.ts
+++ src/01/solution.test.ts
@@ -1,7 +1,7 @@
-import { countIncreases } from './solution';
+import { countIncreases, countIncreasesInThrees } from './solution';
 import fs = require('fs');
 
-describe('day 1', () => {
+describe('day 1a', () => {
     it('should return 0 if only 1 level of depth', () => {
         const count = countIncreases([100]);
         expect(count).toEqual(0);
@@ -20,4 +20,30 @@ describe('day 1', () => {
         const intArray = data.map((v) => parseInt(v))
         expect(countIncreases(intArray)).toEqual(1583);
     });
-})
+});
+
+describe('day 1b', () => {
+    it('should return 0 if array size less than 4', () => {
+        expect(countIncreasesInThrees([1])).toEqual(0);
+        expect(countIncreasesInThrees([1, 2])).toEqual(0);
+        expect(countIncreasesInThrees([1, 2, 3])).toEqual(0);
+    });
+
+    it('should return 1 if first element is less than fourth in array size of 4', () => {
+        expect(countIncreasesInThrees([1, 2, 3, 4])).toEqual(1);
+    });
+
+    it('should return 2 if array has 5 consecutively increasing numbers', () => {
+        expect(countIncreasesInThrees([1, 2, 3, 4, 5])).toEqual(2);
+    });
+
+    it('should return 1 if array has 4 consecutively increasing numbers followed by 1 smallest number', () => {
+        expect(countIncreasesInThrees([1, 2, 3, 4, 0])).toEqual(1);
+    });
+
+    it('should return 1627 with input file', () => {
+        const data = fs.readFileSync('./src/01/input.txt').toString().split('\n');
+        const intArray = data.map((v) => parseInt(v))
+        expect(countIncreasesInThrees(intArray)).toEqual(1627);
+    });
+});
--- src/01/solution.ts
+++ src/01/solution.ts
@@ -1,11 +1,22 @@
-import fs = require('fs');
-import path = require('path');
-
 export const countIncreases = (depths: number[]) => {
     let increases = 0;
 
     for (let i = 0; i < depths.length; i++) {
         if (i > 0 && depths[i] > depths[i - 1]) {
+            increases++;
+        }
+    }
+    return increases;
+}
+
+export const countIncreasesInThrees = (depths: number[]) => {
+    if (depths.length < 4) {
+        return 0;
+    }
+    let increases = 0;
+
+    for (let i = 3; i < depths.length; i++) {
+        if (depths[i] > depths[i-3]) {
             increases++;
         }
     }
