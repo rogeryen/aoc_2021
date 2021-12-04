4d2a7869adb2402167c0b18e35884d36df309f29
Roger yen <rogeryen@gmail.com>
2021-12-04 13:30:27 -0500

    day 3

--- src/03/solution.test.ts
+++ src/03/solution.test.ts
@@ -1,4 +1,4 @@
-import { calculatePowerConsumption } from './solution';
+import { calculateLifeSupportRating, calculatePowerConsumption, findRating, RatingType } from './solution';
 import { readFileIntoStringArray } from '../utils/files';
 
 describe('day 3a', () => {
@@ -20,3 +20,35 @@ describe('day 3a', () => {
         expect(calculatePowerConsumption(data)).toEqual(2954600);
     })
 });
+
+describe('day 3b', () => {
+    it('should return the only number as oxygen generator rating if input is array of one', () => {
+        const oxygenRating = findRating(['1010'], RatingType.Oxygen);
+        expect(oxygenRating).toEqual('1010');
+    });
+
+    it('should return 1111 as oxygen rating if input has three elements with 1 as common bit', () => {
+        expect(findRating(['1010', '0101', '1111'], RatingType.Oxygen)).toEqual('1111');
+    });
+
+    it('should return only number as co2 rating if input is array of one', () => {
+        expect(findRating(['1010'], RatingType.Co2)).toEqual('1010');
+    });
+
+    it('should return 0101 as oxygen rating if input has three elements', () => {
+        expect(findRating(['1010', '0101', '1111'], RatingType.Co2)).toEqual('0101');
+    });
+
+    it('should return only number times itself in decimal as life support rating if input is array of one', () => {
+        expect(calculateLifeSupportRating(['1010'])).toEqual(100);
+    })
+
+    it('should return 1111 * 0101 in decimal as life support rating if input has three elements', () => {
+        expect(calculateLifeSupportRating(['1010', '0101', '1111'])).toEqual(75);
+    })
+
+    it('should return 1662846 as life support rating with input file', () => {
+        const data = readFileIntoStringArray('./src/03/input.txt');
+        expect(calculateLifeSupportRating(data)).toEqual(1662846);
+    })
+})
--- src/03/solution.ts
+++ src/03/solution.ts
@@ -22,8 +22,47 @@ const determineGammaRate = (binaries: string[]):string => {
     return gammaRate.join('');
 } 
 
-export const calculatePowerConsumption = (binaries: string[]):number => {
+export const calculatePowerConsumption = (binaries: string[]): number => {
     const gammaBinary = determineGammaRate(binaries);
     const episilonBinary = gammaBinary.split('').map((bit) => bit === '0' ? '1' : '0').join('');
     return parseInt(gammaBinary, 2) * parseInt(episilonBinary, 2) + 0;
+}
+
+export enum RatingType {
+    Oxygen = 1,
+    Co2,
+}
+
+export const findRating = (binaries: string[], rating: RatingType): string => {
+    let oxygenRating = binaries[0];
+    let candidates = binaries;
+
+    for (let i = 0; i < candidates[0].length; i++) {
+        if (candidates.length === 1) {
+            break;
+        }
+        const ones : string[] = [];
+        const zeroes: string[] = [];
+        for (const candidate of candidates) {
+            if (candidate.charAt(i) === '1') {
+                ones.push(candidate);
+            } else {
+                zeroes.push(candidate);
+            }
+        }
+        if (rating === RatingType.Oxygen) {
+            candidates = ones.length >= zeroes.length ? ones : zeroes;
+        } else {
+            candidates = ones.length < zeroes.length ? ones : zeroes;
+        }
+        
+        oxygenRating = candidates[0];
+    }
+    return oxygenRating;
+}
+
+export const calculateLifeSupportRating = (binaries: string[]): number => {
+    const oxygenRating = parseInt(findRating(binaries, RatingType.Oxygen), 2);
+    const co2Rating = parseInt(findRating(binaries, RatingType.Co2), 2);
+    return oxygenRating * co2Rating + 0;
 }
