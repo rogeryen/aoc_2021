5cc89182e97b091d8a44d6031b04babd0053693a
Roger Yen <roger.yen@mappedin.ca>
2021-12-15 22:45:27 -0500

    9b

--- src/09/solution.test.ts
+++ src/09/solution.test.ts
@@ -1,4 +1,4 @@
-import { getLowpoints, calculateRiskLevel } from './solution';
+import { getLowpoints, calculateRiskLevel, findBasin, productOfLargestThreeBasins } from './solution';
 import { readFileIntoNumberArrays } from '../utils/files';
 
 describe('9a', () => {
@@ -29,3 +29,41 @@ describe('9a', () => {
         expect(calculateRiskLevel(heightMap)).toEqual(496);
     });
 });
+
+describe('9b', () => {
+    it('findBaisin with [[0,1,2],[2,3,4],[3,4,9]] starting from 0, 0 will return [0,1,2,4,3,4,3,2] and set all elements of input to 9', () => {
+        const input = [
+            [0, 1, 2],
+            [2, 3, 4],
+            [3, 4, 9]
+        ]
+        expect(findBasin(input, 0, 0)).toEqual([0, 1, 2, 4, 3, 4, 3, 2]);
+        expect(input).toEqual([[9, 9, 9], [9, 9, 9], [9, 9, 9]]);
+    });
+
+    it('findBaisin with [[0,1,2],[2,3,4],[3,4,9]] starting from 2, 2 will return [] and input unchanged', () => {
+        const input = [
+            [0, 1, 2],
+            [2, 3, 4],
+            [3, 4, 9]
+        ]
+        expect(findBasin(input, 2, 2)).toEqual([]);
+        expect(input).toEqual([[0, 1, 2], [2, 3, 4], [3, 4, 9]]);
+    });
+
+    it('productOfLargestThreeBasins with sample input should return 1134', () => {
+        const heightMap: number[][] = [
+            [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+            [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+            [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+            [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+            [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+        ];
+        expect(productOfLargestThreeBasins(heightMap)).toEqual(1134);
+    });
+
+    it('productOfLargestThreeBasins with input file should return 902880', () => {
+        const heightMap = readFileIntoNumberArrays('./src/09/input.txt');
+        expect(productOfLargestThreeBasins(heightMap)).toEqual(902880);
+    });
+})
--- src/09/solution.ts
+++ src/09/solution.ts
@@ -26,3 +26,31 @@ export const calculateRiskLevel = (heightMap: number[][]): number => {
     const riskLevel = lowpoints.reduce((sum, curr) => sum + curr + 1, 0);
     return riskLevel;
 }
+
+export const findBasin = (heightMap: number[][], row: number, col: number): number[] => {
+    if (heightMap[row] === undefined || heightMap[row][col] === undefined || heightMap[row][col] === 9) {
+        return [];
+    }
+    let basin = [heightMap[row][col]];
+    heightMap[row][col] = 9;
+    basin = basin.concat(findBasin(heightMap, row, col + 1));
+    basin = basin.concat(findBasin(heightMap, row + 1, col));
+    basin = basin.concat(findBasin(heightMap, row, col - 1));
+    basin = basin.concat(findBasin(heightMap, row - 1, col));
+    return basin;
+}
+
+export const productOfLargestThreeBasins = (heightMap: number[][]): number => {
+    const basinSizes: number[] = [];
+    for (let row = 0; row < heightMap.length; row++) {
+        for (let col = 0; col < heightMap[row].length; col++) {
+            const basin = findBasin(heightMap, row, col);
+            if (basin.length > 0) {
+                basinSizes.push(basin.length);
+            }
+        }
+    }
+
+    basinSizes.sort((a, b) => b - a);
+    return basinSizes[0] * basinSizes[1] * basinSizes[2];
+}
