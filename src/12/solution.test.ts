18ef9d293f1c765ddd3dfd497e7ad529c047bf60
Roger Yen <rogeryen@gmail.com>
2021-12-17 12:02:47 -0500

    day 12b

--- src/12/solution.test.ts
+++ src/12/solution.test.ts
@@ -55,9 +55,68 @@ describe('12a', () => {
         expect(findPaths(data).length).toEqual(226);
     });
 
-    it('findPaths should return array of x for input file', () => {
+    it('findPaths should return array of 4167 for input file', () => {
         const data = readFileIntoStringArray('./src/12/input.txt');
         expect(findPaths(data).length).toEqual(4167);
     });
 })
 
+describe('12b', () => {
+    it('findPaths should return array of 36 for the small sample input', () => {
+        const data = [
+            'start-A',
+            'start-b',
+            'A-c',
+            'A-b',
+            'b-d',
+            'A-end',
+            'b-end',
+        ]
+        expect(findPaths(data, true).length).toEqual(36);
+    });
+
+    it('findPaths should return array of 103 for the medium sample input', () => {
+        const data = [
+            'dc-end',
+            'HN-start',
+            'start-kj',
+            'dc-start',
+            'dc-HN',
+            'LN-dc',
+            'HN-end',
+            'kj-sa',
+            'kj-HN',
+            'kj-dc',
+        ]
+        expect(findPaths(data, true).length).toEqual(103);
+    });
+
+    it('findPaths should return array of 3509 for the large sample input', () => {
+        const data = [
+            'fs-end',
+            'he-DX',
+            'fs-he',
+            'start-DX',
+            'pj-DX',
+            'end-zg',
+            'zg-sl',
+            'zg-pj',
+            'pj-he',
+            'RW-he',
+            'fs-DX',
+            'pj-RW',
+            'zg-RW',
+            'start-pj',
+            'he-WI',
+            'zg-he',
+            'pj-fs',
+            'start-RW',
+        ]
+        expect(findPaths(data, true).length).toEqual(3509);
+    });
+
+    it('findPaths should return array of 98441 for input file', () => {
+        const data = readFileIntoStringArray('./src/12/input.txt');
+        expect(findPaths(data, true).length).toEqual(98441);
+    });
+})
--- src/12/solution.ts
+++ src/12/solution.ts
@@ -12,7 +12,7 @@ class Node {
     }
 }
 
-export const findPaths = (data: string[]): string[] => {
+export const findPaths = (data: string[], visitOneSmallCaveTwice = false): string[] => {
     // create graph
     const nodesMap: { [key: string]: Node } = {};
 
@@ -30,31 +30,51 @@ export const findPaths = (data: string[]): string[] => {
         nodesMap[pathNodes[1]].paths.push(nodesMap[pathNodes[0]]);
     }
 
-    // dfs from 'start'
-    // const paths: string[] = [];
-    const nodeVisitStats: { [key: string]: number } = {};
-    Object.keys(nodesMap).forEach((key) => {
-        nodeVisitStats[key] = 0;
-    })
+    let paths: string[][] = []
+    if (visitOneSmallCaveTwice) {
+        for (const node in nodesMap) {
+            if (!nodesMap[node].bigCave && node !== 'start' && node !== 'end') {
+                const nodeVisitStats: { [key: string]: number } = {};
+                Object.keys(nodesMap).forEach((key) => {
+                    nodeVisitStats[key] = 0;
+                })
 
-    const paths = walkToEnd(nodesMap, nodeVisitStats, nodesMap['start']);
-    return paths.map((path) => path.join(','));
+                paths = paths.concat(walkToEnd(nodesMap, nodeVisitStats, nodesMap['start'], node));
+            }
+        }
+    } else {
+        const nodeVisitStats: { [key: string]: number } = {};
+        Object.keys(nodesMap).forEach((key) => {
+            nodeVisitStats[key] = 0;
+        })
+
+        paths = paths.concat(walkToEnd(nodesMap, nodeVisitStats, nodesMap['start'], ''));
+    }
+    const uniqueStringPaths = [...new Set(paths.map((path) => path.join(',')))];
+    return uniqueStringPaths;
 }
 
-const walkToEnd = (nodesMap: { [key: string]: Node }, nodeVisitStats: { [key: string]: number }, node: Node): string[][] => {
+const walkToEnd = (nodesMap: { [key: string]: Node }, nodeVisitStats: { [key: string]: number }, node: Node, nodeToVisitTwice?: string): string[][] => {
     // success condition
     if (node.name === 'end') {
         return [['end']];
     }
     // backtrack
-    if ((!node.bigCave && nodeVisitStats[node.name] > 0)) {
-        return [[]];
+    if (nodeToVisitTwice) {
+        if ((!node.bigCave && node.name !== nodeToVisitTwice && nodeVisitStats[node.name] > 0) || (!node.bigCave && node.name === nodeToVisitTwice && nodeVisitStats[node.name] > 1)) {
+            return [[]];
+        }
+    } else {
+        if (!node.bigCave && nodeVisitStats[node.name] > 0) {
+            return [[]];
+        }
     }
+
     nodeVisitStats[node.name]++;
     const paths = [];
     for (const pathNode of node.paths) {
         const nodeVisitStatsCopy = { ...nodeVisitStats };
-        const subPaths = walkToEnd(nodesMap, nodeVisitStatsCopy, pathNode);
+        const subPaths = walkToEnd(nodesMap, nodeVisitStatsCopy, pathNode, nodeToVisitTwice);
         for (const subPath of subPaths) {
             // only count branches that end properly
             if (subPath[subPath.length - 1] === 'end') {
